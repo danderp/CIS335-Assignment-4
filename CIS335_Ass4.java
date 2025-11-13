b124dd984696c4f2ef2e08fadffad5aafda87d88
danderp <dannyjr377@gmail.com>
2025-11-12 23:11:27 -0500

    Buggy code with more pass 2 implementation

--- CIS335_Ass4.java
+++ CIS335_Ass4.java
@@ -13,14 +13,24 @@ public class CIS335_Ass4 {
                 return i;
             }
         }
+        System.out.printf("%s not found in mnemonic table\n", mnemonic);
         return -1;
     }
-    public static String opcodeCreation(String hexKey, String binaryFlags, int decimalAddress, int format) {
+    public static boolean isInTable(String mnemonic, String[] mnemonicTable) {
+        boolean bool = false;
+        for (int i=0; i<mnemonicTable.length; i++) {
+            if (mnemonic.compareTo(mnemonicTable[i]) == 0) {
+                bool = true;
+            }
+        }
+        return bool;
+    }
+    public static String objcodeCreation(String hexKey, String binaryFlags, int decimalAddress, int format) {
         String binaryOpcode = "";
         StringBuilder opcodeBuilder = new StringBuilder();
         //convert opkey to binary
         int intermedKey = Integer.parseInt(hexKey, 16);
-        int intermed_len = Integer.toString(intermedKey).length() * 4;
+        int intermed_len = 8;
         StringBuilder binaryKeyBuilder = new StringBuilder();
         String binaryKey = Integer.toBinaryString(intermedKey);
 
@@ -105,6 +115,8 @@ public class CIS335_Ass4 {
             String[][] file_data = new String[lines][10];
             ArrayList<String> SYMTAB = new ArrayList<>();
             ArrayList<Integer> ADDRTAB = new ArrayList<>();
+            ArrayList<Integer> LOCCTR = new ArrayList<>();
+            ArrayList<Integer> commentLines = new ArrayList<>();
             String[] opTable = {
                     "ADD", "ADDF", "ADDR", "AND",
                     "CLEAR", "COMP", "COMPF", "COMPR",
@@ -157,7 +169,31 @@ public class CIS335_Ass4 {
                     3, 2, 3
             };
             int[] opExpectedArgs = {
-
+                1, 1, 2, 1,
+                1, 1, 1, 2,
+                1, 1, 2, 0,
+                0, 0, 1, 1,
+                1, 1, 1, 1,
+                1, 1, 1, 1,
+                1, 1, 1, 1,
+                1, 1, 2, 0,
+                1, 1, 2, 0,
+                2, 2, 0, 1,
+                1, 1, 1, 1,
+                1, 1, 1, 1,
+                1, 1, 1, 1,
+                2, 1, 1, 0,
+                1, 1, 1
+            };
+            String[] hardcodedRegisterNames = {
+                "A", "X", "L", "B",
+                "S", "T", "F", "PC",
+                "SW"
+            };
+            int[] hardcodedRegisterInts = {
+                0, 1, 2, 3,
+                4, 5, 6, 8,
+                9
             };
             File file = new File(file_name);
 
@@ -215,6 +251,7 @@ public class CIS335_Ass4 {
             for (int i=0; i<line_count; i++) {
                 if (!file_data[i][0].isEmpty()) {
                     if (file_data[i][0].charAt(0) == '.') {
+                        commentLines.add(i);
                         intermediate_writer.printf("%s\t\t%s%s\n", file_data[i][0], file_data[i][1], file_data[i][2]);
                     } else {
                         if (i == line_count-1) {
@@ -302,57 +339,76 @@ public class CIS335_Ass4 {
 
                 String opcode = file_data[i][1];
                 if (opcode.compareTo("START") == 0) {
+                    LOCCTR.add(location_counter);
                     location_counter = Integer.parseInt(file_data[i][2]);
+
                 }
                 else if (opcode.compareTo("BYTE") == 0) {
                     if (file_data[i][2].charAt(0) == 'C') {
+                        LOCCTR.add(location_counter);
                         location_counter += file_data[i][2].substring(2,file_data[i][2].length()-1).length();
                     } else {
+                        LOCCTR.add(location_counter);
                         location_counter += 1;
+
                     }
                 }
                 else if (opcode.compareTo("WORD") == 0) {
+                    LOCCTR.add(location_counter);
                     location_counter += 3;
+
                 }
                 else if (opcode.compareTo("RESB") == 0) {
                     location_counter += Integer.parseInt(file_data[i][2]);
+                    LOCCTR.add(location_counter);
                 }
                 else if (opcode.compareTo("RESW") == 0) {
                     location_counter += Integer.parseInt(file_data[i][2]) * 3;
+                    LOCCTR.add(location_counter);
                 }
                 else if (opcode.compareTo("END") == 0) {
+                    LOCCTR.add(location_counter);
                 }
                 else if (opcode.compareTo("BASE") == 0) {
                     base = true;
+                    LOCCTR.add(location_counter);
                 }
                 else if (opcode.compareTo("NOBASE") == 0) {
+                    LOCCTR.add(location_counter);
                 }
                 else {
                     if (opcode.isEmpty()) {
+                        LOCCTR.add(location_counter);
                         continue;
                     }
                     int j;
                     if (!file_data[i][0].isEmpty()) {
                         if (file_data[i][0].charAt(0) == '.') {
+                            LOCCTR.add(location_counter);
+                            commentLines.add(i);
                             continue;
                         }
                     }
                     for (j = 0; j < opTable.length; j++) {
+                        String minus_plus_opcode = opcode;
                         if (opcode.charAt(0) == '+') {
-                            opcode = opcode.substring(1);
+                            minus_plus_opcode = opcode.substring(1);
                             location_counter += 1;
+                            LOCCTR.add(location_counter);
+
                         }
                         //if the opcode at line i is equal to a mnemonic in the table
-                        if (opTable[j].compareTo(opcode) == 0) {
+                        if (opTable[j].compareTo(minus_plus_opcode) == 0) {
                             if (opFormats[j] == 1) {
                                 location_counter += 1;
-                            }
-                            else if (opFormats[j] == 2) {
+                                LOCCTR.add(location_counter);
+                            } else if (opFormats[j] == 2) {
                                 location_counter += 2;
-                            }
-                            else if (opFormats[j] == 3) {
+                                LOCCTR.add(location_counter);
+                            } else if (opFormats[j] == 3) {
 
                                 location_counter += 3;
+                                LOCCTR.add(location_counter);
                             } else {
                                 System.out.printf("Error: Format not available for %s\n", opcode);
                                 System.exit(402);
@@ -366,22 +422,146 @@ public class CIS335_Ass4 {
                     }
                 }
             }
-
-
-            System.out.printf("TEST: %s\n", opcodeCreation(opKeys[getKeyIndex("STL", opTable)], "110010", 45, 3));
-
             intermediate_writer.printf("\nSymbol Table: (size %d)\n", SYMTAB.size());
             for (int i=0; i< SYMTAB.size(); i++) {
                 intermediate_writer.println(SYMTAB.get(i));
             }
             intermediate_writer.close();
+
+            //pass 2
+            for (int i = 0; i<line_count; i++) {
+                //check if the line is a comment line
+                boolean isComment = false;
+                for (int c=0; c<commentLines.size(); c++) {
+                    if (i == commentLines.get(c)) {
+                      isComment = true;
+                    }
+                }
+                if (isComment) {
+                    continue;
+                }
+                //variable init
+                String label = file_data[i][0];
+                String opcode = file_data[i][1];
+                int opcodeIndex = getKeyIndex(opcode, opTable);
+                String operand = file_data[i][2];
+                String nixbpe = "000000";
+                int target_address = 0;
+                int format = 0;
+                System.out.println(LOCCTR.size());
+                int program_counter = LOCCTR.get(line_count-1);
+                System.out.printf("Line %d\n", i);
+                if (opcode.charAt(0) == '+') {
+
+                    if (isInTable(opcode.substring(1), opTable)) {
+                        if (operand.isEmpty()) {
+                            nixbpe = "110000";
+                            format = 3;
+                        } else if (opFormats[getKeyIndex(opcode.substring(1), opTable)] == 1) {
+                            format = 1;
+                        } else if (opFormats[getKeyIndex(opcode.substring(1), opTable)] == 2) {
+                            format = 2;
+                        } else //(opFormats[getKeyIndex(opcode, opTable)] == 3)
+                        {
+                            //if the operand is empty
+                            if (operand.charAt(0) == '#') {
+                                for (int j = 0; j < SYMTAB.size(); j++) {
+                                    if (operand.substring(1).compareTo(SYMTAB.get(j)) == 0) {
+                                        target_address = ADDRTAB.get(j);
+                                        break;
+                                    }
+                                }
+                                if ((0 <= target_address) && (target_address <= 4095)) {
+                                    nixbpe = "010000";
+                                    format = 3;
+                                } else if ((4096 <= target_address) && (target_address <= 1048575) && (opcode.charAt(0) == '+')) {
+                                    nixbpe = "010001";
+                                    format = 4;
+                                } else {
+                                    System.out.println("Error: Immediate number out of range");
+                                    System.exit(405);
+                                }
+                            } else {
+                                for (int j = 0; j < SYMTAB.size(); j++) {
+                                    if (operand.substring(1).compareTo(SYMTAB.get(j)) == 0) {
+                                        target_address = ADDRTAB.get(j);
+                                        break;
+                                    }
+                                }
+                                if (opcode.charAt(0) == '+') {
+                                    nixbpe = "110001";
+                                    format = 4;
+                                    System.out.println("test\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+                                } else if ((-2048 <= target_address - program_counter) && (target_address - program_counter <= 2047)) {
+
+                                }
+                            }
+                        }
+                    }
+                }
+                else {
+                    if (isInTable(opcode, opTable)) {
+                        if (operand.isEmpty()) {
+                            nixbpe = "110000";
+                            format = 3;
+                        } else if (opFormats[getKeyIndex(opcode, opTable)] == 1) {
+                            format = 1;
+                        } else if (opFormats[getKeyIndex(opcode, opTable)] == 2) {
+                            format = 2;
+                        } else //(opFormats[getKeyIndex(opcode, opTable)] == 3)
+                        {
+                            //if the operand is empty
+                            if (operand.charAt(0) == '#') {
+                                for (int j = 0; j < SYMTAB.size(); j++) {
+                                    if (operand.substring(1).compareTo(SYMTAB.get(j)) == 0) {
+                                        target_address = ADDRTAB.get(j);
+                                        break;
+                                    }
+                                }
+                                if ((0 <= target_address) && (target_address <= 4095)) {
+                                    nixbpe = "010000";
+                                    format = 3;
+                                } else if ((4096 <= target_address) && (target_address <= 1048575) && (opcode.charAt(0) == '+')) {
+                                    nixbpe = "010001";
+                                    format = 4;
+                                } else {
+                                    System.out.println("Error: Immediate number out of range");
+                                    System.exit(405);
+                                }
+                            } else {
+                                for (int j = 0; j < SYMTAB.size(); j++) {
+                                    if (operand.substring(1).compareTo(SYMTAB.get(j)) == 0) {
+                                        target_address = ADDRTAB.get(j);
+                                        break;
+                                    }
+                                }
+                                if (opcode.charAt(0) == '+') {
+                                    nixbpe = "110001";
+                                    format = 4;
+                                    System.out.println("test\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+                                } else if ((-2048 <= target_address - program_counter) && (target_address - program_counter <= 2047)) {
+
+                                }
+                            }
+                        }
+                    }
+                }
+                if (opcodeIndex != -1) {
+                    System.out.printf("TEST: %s\n", objcodeCreation(opKeys[opcodeIndex], nixbpe, target_address, format));
+                }
+            }
             /*
+
+             */
+
             System.out.println();
             System.out.printf("Address Table: (size %d)\n", ADDRTAB.size());
             for (int i=0; i< ADDRTAB.size(); i++) {
                 System.out.println(ADDRTAB.get(i));
             }
-             */
+
         } catch (FileNotFoundException e) {
             System.out.print("Error: File not found\n");
         } catch (UnsupportedEncodingException e) {
